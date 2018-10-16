ee6857a6349117cbd0c20689cc348fea22eacdc9
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2018-10-16 16:53:22 -0300

    implementing post component insert (without redux)

--- src/posts/post.component.js
+++ src/posts/post.component.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
 export default class PostComponent extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = { new_post: '', posts: [] };
+    }
+
+    change (ev) {
+        this.setState({
+            new_post: ev.target.value
+        });
+    }
+
+    add () {
+        if (this.state.new_post.length === 0) return ;
+        this.setState((prevState) => {
+            return {
+                new_post: '',
+                posts: prevState.posts.concat([this.state.new_post])
+            };
+        });
+    }
+
     render () {
         return <div className="posts">
             <label htmlFor="new_post">Post:</label>
-            <input type="text" name="new_post" value="" />
+            <input type="text" name="new_post"
+                   value={this.state.new_post}
+                   onChange={this.change.bind(this)} />
+            <button onClick={this.add.bind(this)}>add...</button>
             <ul>
+            {this.state.posts.map((post, index) => {
+                return <li key={index}>{post}</li>
+            })}
             </ul>
         </div>;
     }
--- src/posts/post.component.test.js
+++ src/posts/post.component.test.js
@@ -6,7 +6,7 @@ import PostComponent from './post.component';
 it('renders without crashing', () => {
     const div = document.createElement('div');
     let component = <PostComponent />,
-        divs = null, ul = null, input = null;
+        divs = null, ul = null, input = null, button = null;
     ReactDOM.render(component, div);
     divs = div.querySelectorAll('div.posts');
     expect(divs.length).toBe(1);
@@ -14,4 +14,62 @@ it('renders without crashing', () => {
     expect(input .length).toBe(1);
     ul = div.querySelectorAll('div.posts ul');
     expect(ul.length).toBe(1);
+    button = div.querySelectorAll('div.posts button');
+    expect(button.length).toBe(1);
+});
+
+it('includes new posts', () => {
+    const div = document.createElement('div');
+    let component = <PostComponent />,
+        button = null, input = null, li = null;
+    ReactDOM.render(component, div);
+    input = div.querySelector('input');
+    button = div.querySelector('button');
+    li = div.querySelectorAll('li');
+    expect(li.length).toBe(0);
+    input.value = 'Something to be done...';
+    ReactTestUtils.Simulate.change(input);
+    ReactTestUtils.Simulate.click(button);
+    li = div.querySelectorAll('li');
+    expect(li.length).toBe(1);
+    expect(li[0].textContent).toBe('Something to be done...');
+    expect(input.value).toBe('');
+});
+
+it('includes more posts', () => {
+    const div = document.createElement('div');
+    let component = <PostComponent />,
+        button = null, input = null, li = null;
+    ReactDOM.render(component, div);
+    input = div.querySelector('input');
+    button = div.querySelector('button');
+    li = div.querySelectorAll('li');
+    expect(li.length).toBe(0);
+    input.value = 'Pep';
+    ReactTestUtils.Simulate.change(input);
+    ReactTestUtils.Simulate.click(button);
+    input.value = 'Mam';
+    ReactTestUtils.Simulate.change(input);
+    ReactTestUtils.Simulate.click(button);
+    li = div.querySelectorAll('li');
+    expect(li.length).toBe(2);
+    expect(li[0].textContent).toBe('Pep');
+    expect(li[1].textContent).toBe('Mam');
+    expect(input.value).toBe('');
+});
+
+it('should not include empty posts', () => {
+    const div = document.createElement('div');
+    let component = <PostComponent />,
+        button = null, input = null, li = null;
+    ReactDOM.render(component, div);
+    input = div.querySelector('input');
+    button = div.querySelector('button');
+    li = div.querySelectorAll('li');
+    expect(li.length).toBe(0);
+    input.value = '';
+    ReactTestUtils.Simulate.change(input);
+    ReactTestUtils.Simulate.click(button);
+    li = div.querySelectorAll('li');
+    expect(li.length).toBe(0);
 });
